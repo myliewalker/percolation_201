ef2637a2e2ea89d8dd97197bfbe5e5183f75f64f
Mylie Walker <myliewalker@Mylies-MacBook-Pro.local>
2018-12-01 15:59:37 -0500

    updated

--- src/PercolationDFSFast.java
+++ src/PercolationDFSFast.java
@@ -7,9 +7,12 @@ public class PercolationDFSFast extends PercolationDFS {
 	
 	@Override
 	public void updateOnOpen(int row, int col) {
-		if (row == 0) dfs(row, col);
 		int[] deltaR = {-1, 1, 0, 0};
 		int[] deltaC = {0, 0, -1, 0};
+		if (row == 0) {
+			dfs(row, col);
+			return;
+		}
 		for (int i = 0; i < deltaR.length; i++) {
 			if (inBounds(row+deltaR[i], col+deltaC[i]) && 
 				isFull(row+deltaR[i], col+deltaC[i])) {
