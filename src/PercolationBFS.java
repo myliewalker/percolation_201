b9b4d1ea64726a87382e67fd0c81fdac8c610e0d
Mylie Walker <myliewalker@Mylies-MacBook-Pro.local>
2018-12-03 00:37:57 -0500

    calls dfs recursively

--- src/PercolationBFS.java
+++ src/PercolationBFS.java
@@ -16,24 +16,16 @@ public class PercolationBFS extends PercolationDFSFast {
 		Queue<Integer> filled = new LinkedList<>();
 		int[] deltaR = {-1, 1, 0, 0};
 		int[] deltaC = {0, 0, -1, 1};
-		//Assignment description says to dequeue the cell??
-//		for (int i = 0; i < deltaR.length; i++) {
-//			if (inBounds(row+deltaR[i], col+deltaC[i]) && 
-//				isOpen(row+deltaR[i], col+deltaC[i])) {
-//					if (!connected.contains((row+deltaR[i])*myGrid.length + col+deltaC[i]))
-//						connected.add((row+deltaR[i])*myGrid.length + col+deltaC[i]);
-//			}
-//		}
-		
-		 while (connected.size() > 0){
-			 int[] cell = {connected.peek()/myGrid.length, connected.remove()%myGrid.length};
-			 for(int k = 0; k < deltaR.length; k++){
-				 row = cell[0] + deltaR[k];
-				 col = cell[1] + deltaC[k];
-				 if (inBounds(row,col) && isOpen(row, col) && !isFull(row, col)){
-					 filled.add(row*myGrid.length + col);
-				 }
-			 }
-		 }
+		while (connected.size() > 0){
+			int[] cell = {connected.peek()/myGrid.length, connected.remove()%myGrid.length};
+			for(int k = 0; k < deltaR.length; k++){
+				row = cell[0] + deltaR[k];
+				col = cell[1] + deltaC[k];
+				if (inBounds(row,col) && isOpen(row, col) && !isFull(row, col)){
+					filled.add(row*myGrid.length + col);
+					dfs(row, col);
+				}
+			}
+		}
 	}
 }
