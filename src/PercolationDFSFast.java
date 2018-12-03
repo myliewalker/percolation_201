fc21ab06bdc52f650771ab7b956571d7cc768f40
Mylie Walker <myliewalker@Mylies-MacBook-Pro.local>
2018-12-03 00:54:12 -0500

    checks all directions

--- src/PercolationDFSFast.java
+++ src/PercolationDFSFast.java
@@ -8,7 +8,7 @@ public class PercolationDFSFast extends PercolationDFS {
 	@Override
 	protected void updateOnOpen(int row, int col) {
 		int[] deltaR = {-1, 1, 0, 0};
-		int[] deltaC = {0, 0, -1, 0};
+		int[] deltaC = {0, 0, -1, 1};
 		for (int i = 0; i < deltaR.length; i++) {
 			if (row == 0 || (inBounds(row+deltaR[i], col+deltaC[i]) && 
 				isFull(row+deltaR[i], col+deltaC[i]))) {
